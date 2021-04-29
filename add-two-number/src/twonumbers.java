aabc7dd924a5acf2f171681b05d786ef83754cb5
vikasdoddamani <83420516+vikasdoddamani@users.noreply.github.com>
2021-04-29 23:51:19 +0530

    java-file-to-add-two-number

---
+++ add-two-number/src/twonumbers.java
@@ -0,0 +1,17 @@
+
+public class twonumbers {
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int sum;
+
+
+		sum=a+b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("the sum of numbers are= " +sum);
+	
+	}
+
+}
