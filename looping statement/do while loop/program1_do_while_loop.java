d792b496463cd6d2bbf0bc13a4f9f615045fa041
vikasdoddamani <83420516+vikasdoddamani@users.noreply.github.com>
2021-05-02 00:31:47 +0530

    looping statement for, while, do while

---
+++ looping statement/do while loop/program1_do_while_loop.java
@@ -0,0 +1,16 @@
+
+
+// (co-of https://github.com/vikasdoddamani)
+// java program to count the number from 1 to 10
+// from more information read theory_whle_loop
+
+public class program1_do_while_loop {
+  public static void main(String[] args) {
+    int i = 0;
+    do {
+      System.out.println(i);
+      i++;
+    }
+    while (i < 5);  
+  }
+}
---
+++ looping statement/for loop/program1_for_loop.java
@@ -0,0 +1,16 @@
+
+// (co-of https://github.com/vikasdoddamani)
+// java program to count the number from 1 to 10
+// from more information read theory_for_loop
+
+
+class program1_for_loop
+{
+	public static void main(String[] args) {
+
+		for(int i=0;i<=10;i++)                         // for loop means from where you want to start and from where you want to end
+			System.out.println(i);
+		
+	}
+	
+}
