1413c7be9376c2b4fcbaa3112f14245321d0d8cb
vikasdoddamani <83420516+vikasdoddamani@users.noreply.github.com>
2021-05-02 17:13:02 +0530

    NON-parameterized and parameterized constructor

---
+++ Java Constructors/NON-parameterized constructor/program1_cons.java
@@ -0,0 +1,16 @@
+ // (co-of https://github.com/vikasdoddamani)
+// for more information read theory_work txt file
+// Java program to display example of NON-parameterized constructor.
+
+public class program1_cons {
+
+	public static void student()
+	{
+		int age=16;
+		System.out.println("my age is  "+age);
+	}
+	public static void main(String[] args) {
+		student();
+	}
+  
+}
---
+++ Java Constructors/parameterized constructor/program1_const.java
@@ -0,0 +1,16 @@
+ // (co-of https://github.com/vikasdoddamani)
+// for more information read theory_work txt file
+// Java program to display example of parameterized constructor.
+
+public class program1_const {
+
+	public static void student(int age)       // here we init a variable is called  parameterized
+	{
+		
+		System.out.println("my age is  "+age);
+	}
+	public static void main(String[] args) {
+		student(16);                // in parameterized constructor we will reffer a input value here for line no 7
+	}
+  
+}
